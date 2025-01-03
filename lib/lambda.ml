4ed6826b3f44ee640305d25b0f1da7c7c21a9969
Mustafa Aamir <mustafa.290101@gmail.com>
2025-01-03 15:26:27 +0400

    added string_of_term and string_of_type

--- lib/lambda.ml
+++ lib/lambda.ml
@@ -9,6 +9,24 @@ type term =
   | App of term * term (* f x *)
   | Int of int (* useit like Int 42 *)
 
+let rec string_of_type = function
+  | TVar s -> s
+  | TInt -> "int"
+  | TArrow (t1, t2) -> (
+      match t1 with
+      | _ -> string_of_type t1 ^ " -> " ^ string_of_type t2)
 
+
+
+(* does it handle nesting porpely? *)
+let rec string_of_term = function
+  | Var x -> x
+  | Int n -> string_of_int n
+  | Abs (x, t, body) ->
+      "λ" ^ x ^ ": " ^ string_of_type t ^ ". " ^ string_of_term body
+  | App (t1, t2) -> (
+      match t2 with
+      | App (_, _) -> string_of_term t1 ^ " (" ^ string_of_term t2 ^ ")"
+      | _ -> string_of_term t1 ^ " " ^ string_of_term t2)
 
 
