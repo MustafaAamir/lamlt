ccea8f799198fb2e7a62d423a7c262979c733a23
Mustafa Aamir <mustafa.290101@gmail.com>
2025-01-03 18:27:26 +0400

    js format

--- test/test_lcaml.ml
+++ test/test_lcaml.ml
@@ -3,17 +3,20 @@ open Lcaml.Lambda
 open Lcaml.Church
 
 (* reduction test *)
-let rtest name input expected = name >:: fun _ -> assert_equal expected (beta_reduce input) ~printer:string_of_term
+let rtest name input expected =
+  name >:: fun _ -> assert_equal expected (beta_reduce input) ~printer:string_of_term
+;;
 
 (* basic test suite . only contains id*)
 let basic_reduction_tests =
-    "basic_reduction" >::: [
-        (* Identity function application *)
-        rtest "identity function"
-        (App (Abs ("x", TInt, Var "x"), Church.generate 4))
-        (Church.generate 4);
-    ]
-
+  "basic_reduction"
+  >::: [ (* Identity function application *)
+         rtest
+           "identity function"
+           (App (Abs ("x", TInt, Var "x"), Church.generate 4))
+           (Church.generate 4)
+       ]
+;;
 
 (* arithmetic tests *)
 (* logic tests *)
@@ -23,10 +26,5 @@ let basic_reduction_tests =
 (* advancd typecheck tests *)
 (* Alpha Conversion - scoping tests*)
 
-let suite =
-    "all tests" >::: [
-        basic_reduction_tests;
-    ]
-
-
+let suite = "all tests" >::: [ basic_reduction_tests ]
 let () = run_test_tt_main suite
