3441d307391a4903964839ae8cb75cac1f37dd5d
Yusuf <111415081+yusuf-bot@users.noreply.github.com>
2025-01-05 20:42:47 +0400

    Update church.mli

--- lib/church.mli
+++ lib/church.mli
@@ -1,6 +1,15 @@
 module Church : sig
   val true' : Lambda.term
   val false' : Lambda.term
+  val zero : Lambda.term
+  val interpret : Lambda.term -> int
+  val generate : int -> Lambda.term
+  val succ' : Lambda.term
+  val add' : Lambda.term
+  val mul' : Lambda.term
+  val pred' : Lambda.term
+  val if' : Lambda.term
+  val is_zero : Lambda.term
   val and' : Lambda.term
   val or' : Lambda.term
   val not' : Lambda.term
@@ -11,13 +20,8 @@ module Church : sig
   val lt' : Lambda.term
   val gt' : Lambda.term
   val eq' : Lambda.term
-  val zero : Lambda.term
-  val interpret : Lambda.term -> int
-  val generate : int -> Lambda.term
-  val succ' : Lambda.term
-  val add' : Lambda.term
-  val mul' : Lambda.term
-  val pred' : Lambda.term
-  val if' : Lambda.term
-  val is_zero : Lambda.term
+  val y' : Lambda.term
+  val i' : Lambda.term
+  val k' : Lambda.term 
+  val s' : Lambda.term 
 end
